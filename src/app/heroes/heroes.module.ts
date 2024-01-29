124a817fd6836dd490cf76b4fc3a5bc3b79ff494
Jose Ramirez <jramireztolentino@gmail.com>
2024-01-29 14:09:01 -0400

    Add hero Imagen Pipe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 
 @NgModule({
@@ -20,6 +21,9 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
+
+    //Pipe
+    HeroImagePipe,
   ],
   imports: [
     CommonModule,

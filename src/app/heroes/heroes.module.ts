1d0d4b071ee7772a7d03f78241163e8391457abb
Jose Ramirez <jramireztolentino@gmail.com>
2024-01-29 13:50:10 -0400

    Add card component for heroes in list heroes page

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { MaterialModule } from '../material/material.module';
 
+import { CardComponent } from './components/card/card.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
@@ -13,11 +14,12 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
+    CardComponent,
     HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
   imports: [
     CommonModule,

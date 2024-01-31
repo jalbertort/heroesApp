33a26e7ffc5a52e12acb7b528d24bb3b92cbb300
Jose Alberto Ramirez <33133680+jalbertort@users.noreply.github.com>
2024-01-30 23:12:28 -0400

    Feat/frontend (#3)
    * Add forntend login page and register page
    
    * Add json-server and data json
    
    * Add config environments in angular.json
    
    * Add service heroes
    
    * Add card component for heroes in list heroes page
    
    * Add hero Imagen Pipe
    
    * Add loadin herobyid
    
    * Add search hero
    
    * Add frontend new hero

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' }
+  ]
+
 }
